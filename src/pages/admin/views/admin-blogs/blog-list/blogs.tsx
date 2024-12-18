f494bd9687cbeead22bf0d113c4d5f3bf98ab16d
Dachi davituri <davituridachi33@gmail.com>
2024-12-18 15:36:37 +0400

    fix blog list descriptions

--- src/pages/admin/views/admin-blogs/blog-list/blogs.tsx
+++ src/pages/admin/views/admin-blogs/blog-list/blogs.tsx
@@ -3,17 +3,53 @@ import { useGetBlogs } from "../../../../../react-query/query/blogs";
 import { formatDate } from "../../../../../lib/formatDate";
 import { EditOutlined, PlusCircleOutlined } from "@ant-design/icons";
 import { useNavigate } from "react-router";
+import { useState } from "react";
 const { Column } = Table;
+
 const AdminBlogsView: React.FC = () => {
   const navigate = useNavigate();
   const { data: blogsList } = useGetBlogs();
   console.log(blogsList);
+
   const handleNavigateBlogCreate = () => {
     navigate("/dashboard/admin/blog-create");
   };
+
   const handleNavigateBlogEdit = (id: number) => {
     navigate(`/dashboard/admin/blog-update/${id}`);
   };
+
+  const [expandedGeoRows, setExpandedGeoRows] = useState<Set<number>>(
+    new Set()
+  );
+  const [expandedEngRows, setExpandedEngRows] = useState<Set<number>>(
+    new Set()
+  );
+
+  const toggleGeoRowExpansion = (id: number) => {
+    setExpandedGeoRows((prevExpandedRows) => {
+      const newExpandedRows = new Set(prevExpandedRows);
+      if (newExpandedRows.has(id)) {
+        newExpandedRows.delete(id);
+      } else {
+        newExpandedRows.add(id);
+      }
+      return newExpandedRows;
+    });
+  };
+
+  const toggleEngRowExpansion = (id: number) => {
+    setExpandedEngRows((prevExpandedRows) => {
+      const newExpandedRows = new Set(prevExpandedRows);
+      if (newExpandedRows.has(id)) {
+        newExpandedRows.delete(id);
+      } else {
+        newExpandedRows.add(id);
+      }
+      return newExpandedRows;
+    });
+  };
+
   return (
     <Table
       title={() => {
@@ -29,21 +65,11 @@ const AdminBlogsView: React.FC = () => {
       }}
       bordered
       dataSource={blogsList}
-      pagination={false}
       rowKey="id"
+      scroll={{ x: true }}
     >
-      <Column title="User_id" dataIndex="user_id" />
       <Column
-        title="Created at"
-        dataIndex="created_at"
-        render={(created_at) => formatDate(created_at)}
-      />
-      <Column title="Title" dataIndex="title_ka" />
-      {/* <Column title="Title" dataIndex="title_en" /> translate this */}
-      <Column title="Description" dataIndex="description_ka" />
-      {/* <Column title="Description" dataIndex="description_en" /> translate this*/}
-      <Column
-        title="Actions"
+        title="Edit"
         render={(_, row) => (
           <EditOutlined
             className="text-yellow-600"
@@ -51,7 +77,51 @@ const AdminBlogsView: React.FC = () => {
           />
         )}
       />
+      <Column title="User_id" dataIndex="user_id" />
+      <Column
+        title="Created at"
+        dataIndex="created_at"
+        render={(created_at) => formatDate(created_at)}
+      />
+      <Column title="Title Geo" dataIndex="title_ka" />
+      <Column title="Title Eng" dataIndex="title_en" />
+      <Column
+        title="Description Geo"
+        dataIndex="description_ka"
+        render={(text, row) => (
+          <div>
+            {expandedGeoRows.has(row.id) ? text : text.slice(0, 100)}
+            <Button
+              onClick={() => toggleGeoRowExpansion(row.id)}
+              className="block"
+              color={expandedGeoRows.has(row.id) ? 'danger' : 'primary'}
+              variant="text"
+            >
+              {expandedGeoRows.has(row.id) ? "Show less" : "See more"}
+            </Button>
+          </div>
+        )}
+      />
+
+      <Column
+        title="Description Eng"
+        dataIndex="description_en"
+        render={(text, row) => (
+          <div>
+            {expandedEngRows.has(row.id) ? text : text.slice(0, 100)}
+            <Button
+              onClick={() => toggleEngRowExpansion(row.id)}
+              className="block"
+              color={expandedEngRows.has(row.id) ? 'danger' : 'primary'}
+              variant="text"
+            >
+              {expandedEngRows.has(row.id) ? "Show less" : "See more"}
+            </Button>
+          </div>
+        )}
+      />
     </Table>
   );
 };
+
 export default AdminBlogsView;
